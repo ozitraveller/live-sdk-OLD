96c7648034f8c1bc46bde9ae5094c8f09532a99f
ozitraveller <ozitraveller@gmail.com>
2019-05-02 06:53:30 +1000

    update Star-Jwm blend

--- blends/star-jwm/rootfs-overlay/usr/bin/touchtoggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curstate=`synclient | grep -i TouchpadOff | sed -e"s/.*= //"`
-if test "$curstate" = "1"; then
-        synclient TouchpadOff=0
-else
-        synclient TouchpadOff=1
-fi
