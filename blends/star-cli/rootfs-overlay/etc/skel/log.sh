0341992c7a8b5d713a41b76ffbd585c27efaeef2
ozitraveller <ozitraveller@gmail.com>
2019-05-08 07:14:48 +1000

    Star fixed live-session

---
+++ blends/star-cli/rootfs-overlay/etc/skel/log.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+tail -100  /var/log/syslog >> slog
+
+tail -100  /var/log/Xorg.0.log >> x0log
+
+#~ tail -100  /var/log/lightdm/lightdm.log  >> llog
+#~ 
+#~ tail -100  /var/log/lightdm/x-0.log >> xlog
+#~ 
+#~ tail -100  /var/log/lightdm/seat0-greeter.log >> glog
+
+dmesg | tail -100 >> dlog
+
+# /usr/share/X11/xorg.cong.d
+
+# debian (ee) failed to load module vesa (module does not exist 0)
+# No drivers available
+# No screens found (EE)
