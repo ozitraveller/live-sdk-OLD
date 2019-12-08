7d67eac383b9a7cbe85e740a026ecec358e46b5a
ozitraveller <ozitraveller@gmail.com>
2019-12-09 06:38:50 +1100

    version 2.1.1

---
+++ extra/common/rootfs-overlay/log.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+tail -100  /var/log/syslog >> slog
+
+tail -100  /var/log/Xorg.0.log >> x0log
+
+tail -100  /var/log/installer/syslog >> instlog
+
+#~ tail -100  /var/log/lightdm/lightdm.log  >> llog
+#~ 
+#~ tail -100  /var/log/lightdm/x-0.log >> xlog
+#~ 
+#~ tail -100  /var/log/lightdm/seat0-greeter.log >> glog
+
+dmesg | tail -100 >> dlog
+
+# /usr/share/X11/xorg.cong.d
+
+# debian (ee) failed to load module vesa (module does not exist 0)
+# No drivers available
+# No screens found (EE)
+
+sudo apt update
+# sudo apt install debconf-utils
+# sudo debconf-get-selections --installer > debconf-select.txt
+# sudo debconf-get-selections >> file
